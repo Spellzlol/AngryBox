34e96b007b76d3026669f1aae2452ce3d90ae1aa
Spellz <spellz53@gmail.com>
2016-08-16 01:18:03 +0300

    Finished task

---
+++ Assets/Scripts/OnClick.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnClick : MonoBehaviour {
+    
+    public GameObject bodyObject;
+    public Vector2 force;
+
+    private Rigidbody2D body;
+
+    void Start() {
+        body = bodyObject.GetComponent<Rigidbody2D>();
+    }
+
+    public void pushObject () {
+        body.AddForce(force);
+    }
+}
